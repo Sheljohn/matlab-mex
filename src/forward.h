bd219819794b45f7e4cc5f8b7b73f57da8f8bd73
Jonathan H <jonatha.hadida@psych.ox.ac.uk>
2018-10-09 19:15:59 +0100

    Cleanup and add comment to explain

--- src/forward.h
+++ src/forward.h
@@ -1,7 +1,22 @@
-#ifndef JMX_FOO_H_INCLUDED
-#define JMX_FOO_H_INCLUDED
+#ifndef JMX_FORWARD_H_INCLUDED
+#define JMX_FORWARD_H_INCLUDED
 
 namespace jmx {
+
+    /**
+     * The following definitions were forwarded from files:
+     *      getters.h
+     *      extractor.h
+     *      creator.h
+     * 
+     * to allow mappable types (see mapping.h/Abstract) to implement
+     * the Extractor / Creator interfaces. Since Struct is a mappable
+     * type itself, the following functions/methods could not be implemented
+     * before its full definition.
+     * 
+     * This is a bit of a mind-bending inheritance dependency; 
+     * it's not pretty, but it works.
+     */
     
     inline Struct get_struct( const mxArray *ms, index_t index ) {
         return Struct(ms, index);
